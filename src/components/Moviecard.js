0c4041e0f1443bb31814742abfcc5e3b7955d3a1
yasyfiana <52844915+yasyfiana@users.noreply.github.com>
2021-01-24 18:22:49 +0700

    Revert "style card"

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -7,9 +7,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import Rating from '@material-ui/lab/Rating';
-import './MovieCard.css'
-// import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 
 const useStyles = makeStyles({
@@ -19,64 +16,37 @@ const useStyles = makeStyles({
     media: {
       height: 140,
     },
-    padd: {
-
-    }
   });
 
 const MovieCard = () => {
     const classes = useStyles();
      return (
-       <div className="grid-container">
-         <div className = "grid-item">
-            <div className = "grid-content">
-                <div className = "poster">
-                  <img src="/poster/archer.jpg"></img>
-                </div>
-            </div>
-            <div className = "grid-content">
-              <div className="title">
-                <h2>ACHER</h2>
-              </div>
-              <Grid container>
-                  <Grid item container>
-                    <Rating name="rate" defaultValue={1} max={1} readOnly />
-                  </Grid>
-                  <Grid item Container>
-                    8.2
-                  </Grid>
-              </Grid>
-            </div>
-         </div>
-       </div>
-      // <Card className={classes.root}>
-      //   <CardActionArea>
-      //     <div className={classes.padd}>
-      //       <CardMedia
-      //           component="img"
-      //           alt="Contemplative Reptile"
-      //           height="140"
-      //           image='/poster/archer.jpg'
-      //           title="Contemplative Reptile"
-      //         />
-      //     </div>
-      //     <CardContent>
-      //       <Typography gutterBottom variant="h5" component="h2">
-      //         ARCHER
-      //       </Typography>
-      //       <Grid container>
-      //         <Grid item container>
-      //           <Rating name="rate" defaultValue={1} max={1} readOnly />
-      //         {/* <Rating name="half-rating-read" defaultValue={1} precision={0.5} readOnly /> */}
-      //         </Grid>
-      //         <Grid item Container>
-      //           8.2
-      //         </Grid>
-      //       </Grid>
-      //     </CardContent>
-      //   </CardActionArea>
-      // </Card>
+        <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image='/poster/archer.jpg'
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+        </CardActions>
+      </Card>
      );
 };
 
 export default MovieCard;
+
+
+
